397d13c9f27ffc0af33cf392932a40c5a09a3af2
Mohammed Tareq Aldarwish <mhd.tareq.aldarwish@gmail.com>
2024-09-22 16:31:13 +0400

    bug fixing

--- js/clients.js
+++ js/clients.js
@@ -1,73 +1,73 @@
 const clients = [
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 1',
-        address: 'Restaurant Address 1'
+        name: 'Le Cinq',
+        address: '31 Avenue George V, 75008 Paris, France'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 2',
-        address: 'Restaurant Address 2'
+        name: 'Eleven Madison Park',
+        address: '11 Madison Ave, New York, NY 10010, USA'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 3',
-        address: 'Restaurant Address 3'
+        name: 'Osteria Francescana',
+        address: 'Via Stella, 22, 41121 Modena MO, Italy'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 4',
-        address: 'Restaurant Address 4'
+        name: 'The French Laundry',
+        address: '6640 Washington St, Yountville, CA 94599, USA'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 5',
-        address: 'Restaurant Address 5'
+        name: 'Narisawa',
+        address: '2 Chome-6-15 Minamiaoyama, Minato City, Tokyo 107-0062, Japan'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 6',
-        address: 'Restaurant Address 6'
+        name: 'Alinea',
+        address: '1723 N Halsted St, Chicago, IL 60614, USA'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 7',
-        address: 'Restaurant Address 7'
+        name: 'Azurmendi',
+        address: 'Barrio Legina s/n, 48195 Larrabetzu, Bizkaia, Spain'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 8',
-        address: 'Restaurant Address 8'
+        name: 'Per Se',
+        address: '10 Columbus Cir, New York, NY 10019, USA'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 1',
-        address: 'Restaurant Address 9'
+        name: 'The Fat Duck',
+        address: 'High St, Bray SL6 2AQ, United Kingdom'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 1',
-        address: 'Restaurant Address 1'
+        name: 'Gaggan',
+        address: '68/1 Soi Langsuan, Ploenchit Road, Lumpini, Bangkok, Thailand'
     },
-]
+];
 
 const featuredClients = [
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 1',
-        address: 'Restaurant Address 1'
+        name: 'Le Bernardin',
+        address: '155 W 51st St, New York, NY 10019, USA'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 2',
-        address: 'Restaurant Address 2'
+        name: 'Restaurant Guy Savoy',
+        address: 'Monnaie de Paris, 11 Quai de Conti, 75006 Paris, France'
     },
     {
         image: `./img/restaurant-${Math.floor(Math.random() * 3) + 1}.webp`,
-        name: 'Restaurant Name 3',
-        address: 'Restaurant Address 3'
+        name: 'Arpège',
+        address: '84 Rue de Varenne, 75007 Paris, France'
     },
-]
+];
 const swiperWrapper = document.querySelector('.swiper-wrapper');
 const featuredClientsWrapper = document.querySelector('.featured-clients');
 const clientNameElement = document.getElementById('client-name');
